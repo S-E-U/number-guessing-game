717761fbfb987040aae4d5b1c518f20d1b521efa
onglo <wertyedouard@gmail.com>
2016-01-27 13:55:30 +0000

    Initial Commit

---
+++ Number Guessing AppTests/Number_Guessing_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Number_Guessing_AppTests.swift
+//  Number Guessing AppTests
+//
+//  Created by Edouard Long on 27/01/2016.
+//  Copyright © 2016 Edouard Long. All rights reserved.
+//
+
+import XCTest
+@testable import Number_Guessing_App
+
+class Number_Guessing_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
